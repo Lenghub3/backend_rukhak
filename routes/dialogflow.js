a0194b2f5a4f9bab8eb28f527f181c1bdea5f303
Tourleng <hourleng9@gmail.com>
2023-11-06 14:46:48 +0700

    remove order

--- routes/dialogflow.js
+++ routes/dialogflow.js
@@ -1,131 +1,226 @@
 const express = require("express");
 const router = express.Router();
 const dialogflow = require("dialogflow");
-const uuid = require("uuid");
-const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const fs = require("fs/promises");
 
 const config = require("../config/keys");
-
 const projectId = config.googleProjectID;
 const sessionId = config.dialogFlowSessionID;
 const languageCode = config.dialogFlowSessionLanguageCode;
 
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
 let order = false;
 let pendingOrder = {
-  orderId: uuid.v4(),
+  orderId: uuid(),
   products: [],
   quantities: [],
   timestamp: new Date().toISOString(),
-}; 
+};
 
-let products = ["Cacao tree","cotton","sun flowers","happy leaf","soybeans","mango tree"]; 
+let products = [
+  "Cacao tree",
+  "cotton",
+  "sun flowers",
+  "happy leaf",
+  "soybeans",
+  "mango tree",
+  "Oak tree",
+  "Apple",
+  "banana",
+  "kiwi",
+  "melon",
+  "mango",
+  "grape"
+];
 
-
-try {
-  const data = fs.readFileSync("/path/to/products.json");
-  products = JSON.parse(data);
-} catch (error) {
-}
-
-function generateOrderID() {
-  return uuid.v4();
-}
-
-function storeOrder(order) {
-  let orders = [];
-
+async function readProductsFromFile() {
   try {
-    const data = fs.readFileSync("/Users/anbschool0015/backend_rukhak/orders.json");
-    orders = JSON.parse(data);
-  } catch (error) {}
+    const data = await fs.readFile("/path/to/products.json");
+    products = JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading products file:", error);
+  }
+}
 
-  orders.push(order);
-
-  fs.writeFileSync(
-    "/Users/anbschool0015/backend_rukhak/orders.json",
-    JSON.stringify(orders, null, 2)
-  );
-
-  console.log("Order stored successfully.");
+async function storeOrder(order) {
+  try {
+    const data = await fs.readFile("/Users/anbschool0015/backend_rukhak/orders.json");
+    const orders = JSON.parse(data);
+    orders.push(order);
+    await fs.writeFile("/Users/anbschool0015/backend_rukhak/orders.json", JSON.stringify(orders, null, 2));
+    console.log("Order stored successfully.");
+  } catch (error) {
+    console.error("Error storing order:", error);
+  }
 }
 
 router.post("/textQuery", async (req, res) => {
+  const { text } = req.body;
+
   const request = {
     session: sessionPath,
     queryInput: {
       text: {
-        text: req.body.text,
-        languageCode: languageCode,
+        text,
+        languageCode,
       },
     },
   };
 
-  const responses = await sessionClient.detectIntent(request);
-  console.log("Detected intent");
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
+  try {
+    const [response] = await sessionClient.detectIntent(request);
+    const result = response.queryResult;
 
-  if (result.intent.displayName === "new.order") {
-    order = true;
-  }
+    console.log("Detected intent");
+    console.log(`Query: ${result.queryText}`);
+    console.log(`Response: ${result.fulfillmentText}`);
 
-  if (result.intent.displayName === "add_order" && order) {
-    const productItems = result.parameters.fields.product.listValue.values;
-    const numbers = result.parameters.fields.number.listValue.values;
-
-    if (productItems.length !== numbers.length) {
-      result.fulfillmentText = "Please provide both product and quantity for each item.";
-    } else {
-      productItems.forEach((item) => pendingOrder.products.push(item.stringValue));
-      numbers.forEach((item) => pendingOrder.quantities.push(item.numberValue));
+    if (result.intent.displayName === "new.order") {
+      order = true;
     }
-  } else if (result.intent.displayName === "add_order" && !order) {
-    result.fulfillmentText = "You cannot add items to the order until a new order is created.";
-  }
-  if (result.intent.displayName === "show.product") {
-    if (products.length > 0) {
-      const productList = products.join("\n"); // Modify the join function to use newline character
-      result.fulfillmentText = `Here are all available products:\n${productList}`;
-    } else {
-      result.fulfillmentText = "No products are available at the moment.";
-    }
-  }
-  
-  if (result.intent.displayName === "order.completed" && pendingOrder.products.length > 0) {
-    storeOrder(pendingOrder);
-    pendingOrder = {
-      orderId: uuid.v4(),
-      products: [],
-      quantities: [],
-      timestamp: new Date().toISOString(),
-    }; 
-    result.fulfillmentText = "Order completed. Your order has been saved.";
-    order = false;
-  }
 
-  res.send(result);
+    if (result.intent.displayName === "add_order" && order) {
+      const productItems = result.parameters.fields.product.listValue.values;
+      const numbers = result.parameters.fields.number.listValue.values;
+
+      if (productItems.length !== numbers.length) {
+        result.fulfillmentText = "Please provide both product and quantity for each item.";
+      } else {
+        productItems.forEach((item, index) => {
+          pendingOrder.products.push(item.stringValue);
+          pendingOrder.quantities.push(numbers[index].numberValue);
+        });
+      }
+    } else if (result.intent.displayName === "add_order" && !order) {
+      result.fulfillmentText = "You cannot add items to the order until a new order is created.";
+    }
+
+    if (result.intent.displayName === "order.remove" && order) {
+      const removeItems = result.parameters.fields.product.listValue.values;
+      const removeNumbers = result.parameters.fields.number.listValue.values;
+
+      console.log("removeItems:", removeItems);
+      console.log("removeNumbers:", removeNumbers);
+
+      removeItems.forEach((item, index) => {
+        const itemIndex = pendingOrder.products.indexOf(item.stringValue);
+        console.log("itemIndex:", itemIndex);
+
+        if (itemIndex !== -1) {
+          const indexToRemove = itemIndex;
+
+          if (removeNumbers.length > index) {
+            const quantityToRemove = removeNumbers[index].numberValue;
+
+            if (quantityToRemove < pendingOrder.quantities[indexToRemove]) {
+              pendingOrder.quantities[indexToRemove] -= quantityToRemove;
+            } else {
+              pendingOrder.products.splice(indexToRemove, 1);
+              pendingOrder.quantities.splice(indexToRemove, 1);
+            }
+          } else {
+            pendingOrder.products.splice(indexToRemove, 1);
+            pendingOrder.quantities.splice(indexToRemove, 1);
+          }
+        }
+      });
+
+      console.log("pendingOrder:", pendingOrder);
+    }
+
+    if (result.intent.displayName === "remove.order" && order) {
+      const removeItems = result.parameters.fields.product.listValue.values;
+      console.log("removeItems:", removeItems);
+
+      removeItems.forEach((item) => {
+        const itemIndex = pendingOrder.products.indexOf(item.stringValue);
+        console.log("itemIndex:", itemIndex);
+
+        if (itemIndex !== -1) {
+          const indexToRemove = itemIndex;
+          pendingOrder.products.splice(indexToRemove, 1);
+          pendingOrder.quantities.splice(indexToRemove, 1);
+        }
+      });
+
+      console.log("pendingOrder:", pendingOrder);
+    }
+
+    console.log(pendingOrder);
+
+    if (result.intent.displayName === "show.product") {
+      if (products.length > 0) {
+        const productList = products.join("\n");
+        result.fulfillmentText = `Here are all available products:\n${productList}`;
+      } else {
+        result.fulfillmentText = "No products are available at the moment.";
+      }
+    }
+
+    if (result.intent.displayName === "order.completed" && pendingOrder.products.length > 0) {
+      const combinedOrder = {};
+
+      pendingOrder.products.forEach((product, index) => {
+        combinedOrder[product] = (combinedOrder[product] || 0) + pendingOrder.quantities[index];
+      });
+
+      pendingOrder.products = Object.keys(combinedOrder);
+      pendingOrder.quantities = Object.values(combinedOrder);
+
+      const productQuantities = pendingOrder.products.map((product, index) => `${product}: ${pendingOrder.quantities[index]}`);
+      const orderDetails = productQuantities.join(", ");
+
+      result.fulfillmentText = `You have ordered\n(${orderDetails}) Do you want to checkout ?`;
+
+      await storeOrder(pendingOrder);
+      pendingOrder = {
+        orderId: uuid(),
+        products: [],
+        quantities: [],
+        timestamp: new Date().toISOString(),
+      };
+
+      order = false;
+    } else if (result.intent.displayName === "order.completed" && pendingOrder.products.length === 0) {
+      result.fulfillmentText = "Bro why are you not order something before checkout";
+    }
+
+    res.send(result);
+  } catch (error) {
+    console.error("Error processing text query:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 router.post("/eventQuery", async (req, res) => {
+  const { event } = req.body;
+
   const request = {
     session: sessionPath,
     queryInput: {
       event: {
-        name: req.body.event,
-        languageCode: languageCode,
+        name: event,
+        languageCode,
       },
     },
   };
 
-  const responses = await sessionClient.detectIntent(request);
-  console.log("Detected intent");
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  res.send(result);
+  try {
+    const [response] = await sessionClient.detectIntent(request);
+    const result = response.queryResult;
+
+    console.log("Detected intent");
+    console.log(`Query: ${result.queryText}`);
+    console.log(`Response: ${result.fulfillmentText}`);
+
+    res.send(result);
+  } catch (error) {
+    console.error("Error processing event query:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
