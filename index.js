4c0a3cc5c1d9d361bbed9e92166436525ad31b29
LENG <leng@LENGs-MacBook-Pro.local>
2023-11-11 11:01:45 +0700

    text2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from "path";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import { Server } from 'socket.io'; // Import the Server class
+// import { Server } from 'socket.io'; // Import the Server class
 import dotenv from "dotenv";
 import dialogflowRoutes from "./routes/dialogflow.js";
 // import notificationsRoutes from "./routes/notification.js";
@@ -28,43 +28,43 @@ db.once("open", () => {
 });
 
 // Setup Socket.IO
-const server = app.listen(port, () => {
-  console.log(`Server Running at ${port}`);
-});
+// const server = app.listen(port, () => {
+//   console.log(`Server Running at ${port}`);
+// });
 
-// Create a Socket.IO instance and pass the existing server with CORS configuration
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173", // This should match the origin of your frontend
-    methods: ["GET", "POST"],
-  },
-});
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
+// // Create a Socket.IO instance and pass the existing server with CORS configuration
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173", // This should match the origin of your frontend
+//     methods: ["GET", "POST"],
+//   },
+// });
+// app.use((req, res, next) => {
+//   req.io = io;
+//   next();
+// });
 
-io.on("connection", (socket) => {
-  console.log("Connected to Socket.IO");
+// io.on("connection", (socket) => {
+//   console.log("Connected to Socket.IO");
 
-  socket.on("orderStored", (data) => {
-    console.log("Product stored:", data.message);
-    io.emit("orderStored", data); // Broadcast the event to all connected clients
-  });
-});
+//   socket.on("orderStored", (data) => {
+//     console.log("Product stored:", data.message);
+//     io.emit("orderStored", data); // Broadcast the event to all connected clients
+//   });
+// });
 
 // Configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Enable CORS for Express
-app.use(
-  cors({
-    origin: "http://localhost:5173", // This should match the origin of your frontend
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173", // This should match the origin of your frontend
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
 
 // Define routes
 app.use("/api/dialogflow", dialogflowRoutes);
